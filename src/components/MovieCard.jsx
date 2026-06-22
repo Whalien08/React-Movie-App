1fe89cefccf36796bea9a714a0f4a341d9d0a103
Whalien08 <mithraxavier.cse@gmail.com>
2026-06-22 20:18:01 +0530

    heart

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -19,7 +19,7 @@ function MovieCard({movie}){
                 <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
                 <div className="movie-overlay">
                     <button className={`favorite-btn ${favorite ? "active" : ""}`} onClick={onFavClick}>
-                       ♡ 
+                       ♥
                     </button>
                 </div>
             </div>
